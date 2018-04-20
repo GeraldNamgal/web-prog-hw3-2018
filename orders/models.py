01dfec59e7749a40bed069b5ea86245f79497dd0
Namgal <35800136+GeraldNamgal@users.noreply.github.com>
2018-04-20 13:37:11 -0400

    Changed the digit limit in Models for bigger prices.

--- orders/models.py
+++ orders/models.py
@@ -11,8 +11,8 @@ class Category(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length=64)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    priceSmall = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
-    priceLarge = models.DecimalField(max_digits=4, decimal_places=2)
+    priceSmall = models.DecimalField(max_digits=11, decimal_places=2, null=True, blank=True)
+    priceLarge = models.DecimalField(max_digits=11, decimal_places=2)
 
     def __str__(self):
         return f'{self.name} ({self.category}): ${self.priceSmall} - ${self.priceLarge}'
@@ -45,10 +45,10 @@ class Order(models.Model):
     itemID = models.IntegerField()
     itemName = models.CharField(max_length=64)
     itemCategory = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
     size = models.CharField(max_length=16, null=True, blank=True)
     quantity = models.IntegerField(default=1)
-    subtotal = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+    subtotal = models.DecimalField(max_digits=11, decimal_places=2, default=0.00)
     dateTime = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     complete = models.BooleanField(default=False)
 
